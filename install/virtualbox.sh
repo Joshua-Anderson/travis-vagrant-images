4633d08c6008f9a46f90abd8ba8cf2111a47ea5d
joshua-anderson <j@zatigo.com>
2014-09-03 17:37:06 -0700

    Fixed relative path issues

--- install/virtualbox.sh
+++ install/virtualbox.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Downloads and installs virtualbox
-curl -o ../dowloads/virtualbox.dmg 'http://download.virtualbox.org/virtualbox/4.3.14/VirtualBox-4.3.14-95030-OSX.dmg'
-hdiutil mount ../dowloads/virtualbox.dmg
+
+SCRIPTDIR=$(dirname $0)
+mkdir -p $SCRIPTDIR/../downloads
+curl -o $SCRIPTDIR/../downloads/virtualbox.dmg 'http://dlc.sun.com.edgesuite.net/virtualbox/4.3.14/VirtualBox-4.3.14-95030-OSX.dmg'
+hdiutil mount $SCRIPTDIR/../downloads/virtualbox.dmg
