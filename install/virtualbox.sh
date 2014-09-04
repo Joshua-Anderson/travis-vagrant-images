b0fd132c0ecf7d4408f8e7ca172fc31d5353a487
Joshua Anderson <j@joshua-anderson.com>
2014-09-03 18:36:17 -0700

    Fix app install location

--- install/virtualbox.sh
+++ install/virtualbox.sh
@@ -5,5 +5,4 @@ SCRIPTDIR=$(dirname $0)
 mkdir -p $SCRIPTDIR/../downloads
 curl -o $SCRIPTDIR/../downloads/virtualbox.dmg 'http://dlc.sun.com.edgesuite.net/virtualbox/4.3.14/VirtualBox-4.3.14-95030-OSX.dmg'
 hdiutil mount $SCRIPTDIR/../downloads/virtualbox.dmg
-sudo installer -package /Volumes/VirtualBox/VirtualBox.pkg -target "/Volumes/Macintosh HD"
-hdiutil unmount $SCRIPTDIR/../downloads/virtualbox.dmg
+sudo installer -package /Volumes/VirtualBox/VirtualBox.pkg -target "/Applications"
