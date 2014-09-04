91cd8edabaf5b71eec206c7eae28230f04da466f
joshua-anderson <j@zatigo.com>
2014-09-03 19:03:40 -0700

    Install Vagrant package

--- install/vagrant.sh
+++ install/vagrant.sh
@@ -5,4 +5,4 @@ SCRIPTDIR=$(dirname $0)
 mkdir -p $SCRIPTDIR/../downloads
 curl -o $SCRIPTDIR/../downloads/vagrant.dmg -L 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.4.dmg'
 hdiutil mount $SCRIPTDIR/../downloads/vagrant.dmg
-#sudo installer -package /Volumes/Vagrant/Vagrant.pkg -target "/"
+sudo installer -package /Volumes/Vagrant/Vagrant.pkg -target "/"
